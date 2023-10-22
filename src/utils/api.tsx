97250d8468d800b9391b5d5b1dbfcdeaaf2cd951
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-10-22 08:09:01 +0800

    hardcode api link

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -4,7 +4,7 @@ function get_base_url() {
   if (process.env.NODE_ENV == 'production') {
     return process.env.API_URL_PROD;
   } else {
-    return process.env.API_URL;
+    return 'https://apis.sipakamaseta.com/api/v1/';
   }
 }
 
