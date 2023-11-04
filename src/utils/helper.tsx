40673860b640be46968a0a5f63131116f5c070a7
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-11-04 20:48:36 +0800

    route prodnya

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -2,7 +2,7 @@ export function getBaseUrl() {
     if (process.env.NODE_ENV == "production") {
         return process.env.API_URL_PROD;
     } else {
-        return process.env.API_URL;
+        return 'https://apis.sipakamaseta.com/';
     }
 }
 
